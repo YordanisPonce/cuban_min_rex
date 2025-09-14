c7abdcfe7dd1b1671de508a3fe4eccd3b35936a3
AZDemonEdge <rene001208@gmail.com>
2025-09-14 16:36:55 -0400

    Add Categories CRUD

---
+++ app/Filament/Resources/Categories/Schemas/CategoryForm.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Filament\Resources\Categories\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Illuminate\Support\Facades\Auth;
+use Filament\Forms\Components\Hidden;
+use Filament\Schemas\Schema;
+
+class CategoryForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Hidden::make('user_id')->default(Auth::user()->id),
+                Hidden::make('is_general')->default(Auth::user()->role == 'admin'),
+                TextInput::make('name')
+                    ->required(),
+            ]);
+    }
+}
