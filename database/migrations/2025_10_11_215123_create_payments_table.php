2e50e65a49f549c06c2dc95cbd0d73a9f49ef065
Yordism <yordisponcealderete@gmail.com>
2025-10-11 20:33:53 -0400

    Update

--- database/migrations/2025_10_11_215123_create_payments_table.php
+++ database/migrations/2025_10_11_215123_create_payments_table.php
@@ -14,13 +14,13 @@ return new class extends Migration
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
-            $table->json('paypal_response');
-            $table->string('item_id');
-            $table->string('sender_batch_id');
-            $table->decimal('amount', 8, 2);
-            $table->string('currency');
-            $table->string('email');
-            $table->string('note');
+            $table->json('paypal_response')->nullable();
+            $table->string('item_id')->nullable();
+            $table->string('sender_batch_id')->nullable();
+            $table->decimal('amount', 8, 2)->nullable();
+            $table->string('currency')->nullable();
+            $table->string('email')->nullable();
+            $table->string('note')->nullable();
             $table->timestamps();
             $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
         });
