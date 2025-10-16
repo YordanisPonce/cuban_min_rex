392284d6fd28646ab2f3669e2abc618db7b62c38
Yordism <yordisponcealderete@gmail.com>
2025-10-16 12:32:38 -0400

    Update

--- app/Filament/Pages/UserPayments.php
+++ app/Filament/Pages/UserPayments.php
@@ -2,35 +2,24 @@
 
 namespace App\Filament\Pages;
 
-use App\Livewire\UserPaymentsTable;
 use App\Models\User;
 use Filament\Pages\Page;
-use Filament\Resources\Pages\Concerns\InteractsWithRecord;
 
 class UserPayments extends Page
 {
-
     protected string $view = 'filament.pages.user-payments';
 
-    protected static ?string $title = ' ';
+    protected static ?string $title = 'Pagos realizados';
 
     protected static ?string $navigationLabel = null; // No mostrar en la navegación
-
+    
     protected static bool $shouldRegisterNavigation = false; // No registrar en la navegación
 
     public User $user;
 
-
-    public function mount(int|string $record): void
+    public function mount($id)
     {
-        $this->user = User::findOrFail($record);
+        $this->user = User::findOrFail($id);
     }
 
-    // Agregar este método para pasar datos a la vista
-    protected function getViewData(): array
-    {
-        return [
-            'userId' => $this->user->id,
-        ];
-    }
 }
