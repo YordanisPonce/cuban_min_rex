6ed508971d9ae4d69bbc7e0d27632d943db2e6a9
Yordism <yordisponcealderete@gmail.com>
2025-12-04 14:23:03 -0500

    Loggin bug with downloads

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -6,6 +6,7 @@ use App\Models\File;
 use App\Models\Order;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Storage;
 use ZipArchive;
@@ -15,41 +16,50 @@ class OrderController extends Controller
     public function download(Request $request, string $id)
     {
         $token = $request->get('token');
+        Log::debug('Download token: ' . $token);
         if ($token) {
+
             $user = User::whereJsonContains('downloadToken', $token)->first();
             $order = Order::find($id);
             if ($user && $order) {
                 $downloadToken = $user->downloadToken;
                 $indice = array_search($token, $downloadToken);
-                unset($downloadToken[$indice]);
+                Log::debug('Download token index: ' . $indice);
+                //unset($downloadToken[$indice]);
                 $user->downloadToken = $downloadToken;
                 $user->save();
 
                 $zip = new ZipArchive();
-                $zipFileName = ''.config('app.name').'-pack.zip';
-                $zipFilePath = storage_path('app/public/files/zip' . $zipFileName);
+                $zipFileName = config('app.name') . '-' . uniqid() . '-pack.zip';
+                $zipFilePath = Storage::path("files/zip/$zipFileName");
+                Log::debug('Creating ZIP file at: ' . $zipFilePath);
 
                 if ($zip->open($zipFilePath, ZipArchive::CREATE | ZipArchive::OVERWRITE) !== TRUE) {
                     return response()->json(['error' => 'No se pudo crear el archivo ZIP'], 500);
                 }
 
+                Log::debug('Adding files to ZIP archive');
                 foreach ($order->order_items as $key => $value) {
                     $file = File::find($value);
+                    Log::debug('Adding file to ZIP: ' . $file->original_file);
                     $path = $file->original_file;
                     if (!Storage::disk('s3')->exists($path)) {
-                        abort(404);
+                        // abort(404);
+                        continue;
                     }
                     $ext = pathinfo($path, PATHINFO_EXTENSION);
                     $downloadName = "$file->name.$ext";
                     $zip->addFile($path, $downloadName);
                 }
+                Log::debug('Closing ZIP archive');
 
                 $zip->close();
 
                 if (!file_exists($zipFilePath)) {
                     return response()->json(['error' => 'El archivo ' . $zipFileName . ' no se ha creado.'], 500);
                 }
-                
+                Log::debug('ZIP file created successfully at: ' . $zipFilePath);
+
                 return Response::download($zipFilePath)->deleteFileAfterSend(true);
             }
             return redirect('/')->with('error', 'Usted no tiene permisos para descargar el archivo seleccionado.');
