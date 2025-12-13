0ad7260eb09661b408c833b24352225af0f75c7a
AZDemonEdge <rene001208@gmail.com>
2025-12-13 15:58:08 -0500

    Add Youtube to contact links

--- config/contact.php
+++ config/contact.php
@@ -3,5 +3,6 @@
 return [
     'email' => env('CONTACT_EMAIL'),
     'phone' => env('CONTACT_PHONE'),
-    'instagram' => env('CONTACT_INSTAGRAM')
+    'instagram' => env('CONTACT_INSTAGRAM'),
+    'youtube' => env('CONTACT_YOUTUBE')
 ];
