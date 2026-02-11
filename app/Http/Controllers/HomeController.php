464b730924e4494f979159f9588ac18c0fd47c95
AZDemonEdge <rene001208@gmail.com>
2026-02-11 16:54:44 -0500

    Fix Radio View

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -173,7 +173,11 @@ class HomeController extends Controller
 
         $setting = Setting::first();
 
-        $activeCUPPayment = !(is_null($setting->credit_card_info) | is_null($setting->confirmation_phone) | is_null($setting->confirmation_email) | is_null($setting->currency_convertion_rate));
+        $activeCUPPayment = '';
+
+        if ($setting) {
+            $activeCUPPayment = !(is_null($setting->credit_card_info) | is_null($setting->confirmation_phone) | is_null($setting->confirmation_email) | is_null($setting->currency_convertion_rate));
+        }
 
         return view('radio', compact('djs', 'categories', 'recentCategories', 'recentDjs', 'lives', 'livesPlayList', 'mixes', 'mixesPlayList', 'allCategories', 'allRemixers', 'activeCUPPayment'));
     }
