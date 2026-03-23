cb96c0ecfc914bb6f071a17ca09e681721eae9e8
AZDemonEdge <rene001208@gmail.com>
2026-03-23 17:49:15 -0400

    Fix images path fix

--- app/Console/Commands/FixImagenPath.php
+++ app/Console/Commands/FixImagenPath.php
@@ -2,33 +2,6 @@
 
 namespace App\Console\Commands;
 
-use Illuminate\Console\Command;
-
-class FixImagenPath extends Command
-{
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'app:fix-imagen-path';
-
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Command description';
-
-    /**
-     * Execute the console command.
-     */
-    public function handle()
-    {
-        <?php
-
-namespace App\Console\Commands;
-
 use App\Models\Collection;
 use App\Models\File;
 use App\Models\Plan;
