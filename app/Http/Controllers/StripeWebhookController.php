003a7ebd2999769ddbfc8ac26a87c9ce2d84e788
AZDemonEdge <rene001208@gmail.com>
2026-04-18 13:54:13 -0400

    Fix Stripe Hook suscription

--- app/Http/Controllers/StripeWebhookController.php
+++ app/Http/Controllers/StripeWebhookController.php
@@ -47,7 +47,7 @@ class StripeWebhookController extends CashierController
                 $order->save();
 
                 $user->current_plan_id = $plan->id;
-                $user->plan_starts_at = Carbon::now();
+                $user->plan_start_at = Carbon::now();
                 $user->plan_expires_at = Carbon::now()->addMonths($plan->duration_months);
                 $user->save();
 
@@ -175,7 +175,7 @@ class StripeWebhookController extends CashierController
                 $user->current_plan_id = (int) $planId;
             }
             $user->plan_expires_at = $periodEnd;
-            $user->plan_starts_at = Carbon::now();
+            $user->plan_start_at = Carbon::now();
             $user->save();
 
             $lastOrder = Order::query()
@@ -227,7 +227,7 @@ class StripeWebhookController extends CashierController
                     $order->save(); 
                     
                     $user->current_plan_id = $plan->id; 
-                    $user->plan_starts_at = Carbon::now();
+                    $user->plan_start_at = Carbon::now();
                     $user->plan_expires_at = now()->addMonths($plan->duration_months); 
                     $user->save(); 
                     
